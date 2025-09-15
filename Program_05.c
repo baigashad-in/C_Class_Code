08d9a8e64e327ba024112c0297053173205a6404
baigashad-in <baigashad.in@gmail.com>
2025-09-15 20:35:34 +0530

    Add files via upload

---
+++ Program_05.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    int b;
+    scanf("%d%d",&a,&b);
+    printf("result = %d + %d = %d",a,b,a+b);
+    getch();
+}
+
+
+// https://www.youtube.com/watch?v=Si8rN5J249M
