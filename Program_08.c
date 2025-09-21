59408c472ebf0c5426fd65a54e3d79d94b89161e
baigashad-in <baigashad.in@gmail.com>
2025-09-21 22:17:50 +0530

    Add files via upload

---
+++ Program_08.c
@@ -0,0 +1,10 @@
+// WAP to find ascii of a character
+#include<stdio.h>
+void main()
+{
+    char alphabet;
+    printf("Enter a single charater : ");
+    scanf("%c", &alphabet);
+    printf("Character = %c ", alphabet);
+    printf("\nASCII of character = %d", alphabet);
+}
