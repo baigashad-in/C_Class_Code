1f1aca687a13cfbe88d43256d898f49e3bc3874e
baigashad-in <baigashad.in@gmail.com>
2025-09-21 22:18:16 +0530

    Add files via upload

---
+++ Program_10.c
@@ -0,0 +1,69 @@
+// WAP to calculate simple interest
+#include<stdio.h>
+void main()
+// {
+//  float principle, rate, time;
+//  printf("Enter Principle amount, rate of interest, time period : ");
+//  scanf("%f%f%f", &principle, &rate, &time);
+//  float interest_earned = (principle * rate * time)/100;
+//  printf("\nSimple Interest Earned = %f", interest_earned);
+// }
+
+
+// 3.	WAP to input km and convert it to the meters
+// {
+//     float km;
+//     printf("Enter Km value : ");
+//     scanf("%f", &km);
+//     printf("Value in meters = %f", km*1000);
+// }
+
+// 4.	WAP to input your 6 subject marks and calculate percentage
+// {
+//     int m1, m2, m3, m4, m5, m6;
+//     printf("Enter marks of Hindi, English, Maths, Science, Arts and Social Science : ");
+//     scanf("%d%d%d%d%d%d", &m1, &m2, &m3, &m4, &m5, &m6);
+//     int total_marks = (m1 + m2 + m3 + m4 + m5 + m6);
+//     float percentage_marks = ((total_marks*100)/(600.0));
+//     printf("Total Marks obtained = %d ", total_marks);
+//     printf("\nPercentage obtained = %f ", percentage_marks); 
+// }
+
+// 5.	WAP to input radius of circle and calculate area of circle
+// {
+//     float radius;
+//     printf("Enter the value of radius of a circle in cm : ");
+//     scanf("%f", &radius);
+//     printf("Area of Circle is = %f", 3.1415926*radius*radius);
+// }
+
+// 6.	WAP to input length and width and calculate area of rectangle (L x W).
+
+// {
+//     float length, width;
+//     printf("Enter values of Length and Width : ");
+//     scanf("%f%f", &length, &width);
+//     printf("Area of rectangle = %f", length*width);
+// }
+
+
+// 7.	WAP to input basic salary and calculate gross salary (gross salary = basic salary + DA + HRA + 5000), DA=10% of basic salary and HRA = 50% of basic salary
+
+// {
+//     float basic_salary;
+//     printf("Enter basic salary amount : ");
+//     scanf("%f", &basic_salary);
+//     float da = basic_salary/10;
+//     float hra = basic_salary/2;
+//     printf("Gross Salary is = %f", basic_salary + da + hra + 5000);
+// }
+
+
+// 8.	WAP to input Indian rupees and convert it to the dollars.
+
+{
+    float inr;
+    printf("Enter Indian Rupees amount : ");
+    scanf("%f", &inr);
+    printf("Dollar Amount = %f", inr*0.012);
+}
