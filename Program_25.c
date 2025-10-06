04ecc70d392f281f8f986cd0c33a2fe2b2d7d655
baigashad-in <baigashad.in@gmail.com>
2025-10-07 01:37:07 +0530

    Add files via upload

---
+++ Program_25.c
@@ -0,0 +1,48 @@
+// WAP to print all numbers that is divisible by 3 within 2 to 50.
+
+#include<stdio.h>
+void main()
+{
+    for(int a = 2; a<=50; a++)
+    {
+        if(a%3==0)
+        {
+        printf("%d\n",a);
+        }
+    }
+}
+
+    // WAP to print numbers that is divisible by 3 and 7 within 1 to 50.
+    // WAP to print all numbers that is divisible by 2 and 3 within 1 to 50.
+    // WAP to print all numbers that ends with 2 in 1 to 100.
+    // WAP to print all numbers that is divisible by 5 and 7 within 2 to to 100.
+    // WAP to print all numbers that is divisible by 3 within 2 to 50.
+// Initial value is not fixed :-
+    // WAP to print all even numbers within n to 100 where n is less than 100.
+    // WAP to print all odd numbers within n to 100 where n is less than 100.
+    // WAP to print all numbers that are divisible by 3 and 5 within n to 100 where n is less than 100.
+    // WAP to print all numbers that are divisible by 10 within n to 100 where n is less than 100.
+    // WAP to print all numbers that are divisible by 7 within n to 100 where n is less than 100.
+// End value is not fixed :-
+    // WAP to print all numbers within 1 to n where n is greater than 1.
+    // WAP to print all even numbers within 1 to n where n is greater than 1.
+    // WAP to print all odd numbers within 1 to n where n is greater than 1.
+    // WAP to print all numbers divisible by 3 and 7 within 1 to n where n is greater than 1.
+// Updation value is not fixed :-
+    // WAP to print 1 4 9 16 25 36 49 64 81 100...
+    // WAP to print 0 1 1 2 3 5 8 13 21 34...
+    // WAP to print 10 9 8 7 6 5 4 3 2 1...
+    // WAP to print 5 10 15 20 25 30 35 40 45 50...
+    // WAP to print 1 2 4 8 16 32 64 128 256 512...
+    // WAP to print 1 -2 3 -4 5 -6 7 -8 9 -10...
+    // WAP to print 1 11 111 1111 11111...
+    // WAP to print 0 1 3 6 10 15 21 28...
+// WAP to print all even numbers within a given range, where given range in n to m where n < m always.
+// WAP to print factorial of a given number.
+// WAP to print sum of all numbers within 1 to 10
+// WAP to check a number is a perfect number or not. (sum of a number's factor = that number. eg. 6 = 1+2+3)
+// WAP to find sum of digits of a number
+// WAP to check if a given number is armstrong number or not.(e.g 1^3 + 5^3 + 1^3 = 153, 1634 = 1^4+6^4+3^4+4^4)
+// WAP to check if a given number a palindrome number or not.
+// WAP to get reverse number of a number.
+// WAP to to print sum of all even numbers within 1 to 100.
