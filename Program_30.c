aa017b76fcc8e3d54acc95326b4a63c5479cd556
baigashad-in <baigashad.in@gmail.com>
2025-10-08 03:40:56 +0530

    Add files via upload

---
+++ Program_30.c
@@ -0,0 +1,20 @@
+// WAP to print sum of each values generated in each iteration
+#include<stdio.h>
+void main()
+{
+    int temp = 0;
+    for(int i =1; i<=10; i = i+1)
+    {
+        temp = temp + i; // Adding value of i generated in each iteration and storing the sum in temp variable.
+    }
+}
+
+// WAP to print multiplication of each values generated in each iteration :
+ 
+// {
+//     int temp = 1;
+//     for(int i =1; i<=10; i = i+1)
+//     {
+//         temp = temp * i; // Multiplying value of i generated in each iteration and storing the multiplication in temp variable.
+//     }
+// }
