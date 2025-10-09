10aab6294fc2dba84ce965e635a0ce0dcc1293c2
baigashad-in <baigashad.in@gmail.com>
2025-10-10 00:12:17 +0530

    Add files via upload

---
+++ Program_34.c
@@ -0,0 +1,16 @@
+// WAP to input a number and print cube of all digits.
+#include<stdio.h>
+void main()
+{
+ int num, digit;
+ printf("Enter a number : ");
+ scanf("%d", &num);
+ int sum = 0;
+ int temp = num;
+ for( ; num > 0; num = num/10) /*initialization is done using scanf*/
+ {
+    digit = num%10;
+    sum = digit*digit*digit;
+    printf("Cube of %d = %d \n", digit, sum);
+ }
+}
