ebec7c3a270c8f6b7f56b5b6e55e0e836016c00d
baigashad-in <baigashad.in@gmail.com>
2025-10-12 04:07:17 +0530

    Add files via upload

---
+++ Program_41.c
@@ -0,0 +1,23 @@
+// WAP to input a number and check whether the number is armstrong number or not.
+
+// #include<stdio.h>
+// #include<math.h>
+// // pow(), sqrt(), abs()
+// void main()
+// {
+//  int num, digit;
+//  printf("Enter a number : ");
+//  scanf("%d", &num);
+//  int count = 0;
+//  int temp = num;
+//  for( ; num > 0; num = num/10) /*initialization is done using scanf*/
+//  {
+//     digit = num%10;
+//     count = count + 1;
+//  }
+
+// //  double result = pow(temp, count);
+
+//  printf("Number of digits in %d = %d \n", temp, count);
+
+// }
