0bd2364f2497d2db29e86146242c42a901d82021
baigashad-in <baigashad.in@gmail.com>
2025-10-15 04:01:21 +0530

    Add files via upload

---
+++ Program_52.c
@@ -0,0 +1,30 @@
+// WAP to print table of all numbers between 1 to 40
+// WAP to print table of all numbers between 1 to n.
+// WAP to print reverse number of all numbers from 101 to 999.
+// WAP to print all palindrome numbers between 101 to 999.
+// WAP to print cube of all numbers between 1 to 50 without using predefined function.
+// WAP to print all perfect numbers between 1 to 100.
+// WAP to print all perfect numbers between 1 to n.
+// WAP to print all prime numbers between 1 to n.
+// WAP to print all armstrong numbers between 1 to 1000.
+// WAP to print all armstrong numbers between 1 to 100
+
+
+#include<stdio.h>
+void main()
+{
+ for(int i = 1; i<=5; i++)   // Outer loop iterates for 6 times
+ {
+    for(int j=1; j<=i; j++)
+    {
+        {
+            // printf("%*s",5-i,"");
+            // printf(" ");
+            // printf("%c\t", '*');
+            printf("%c\t", '*');
+        }
+    }
+    printf("\n");
+ }
+}
+
