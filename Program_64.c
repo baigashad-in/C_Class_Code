58fcbf067eaec38dc9d7ad454747184e48f8aedb
baigashad-in <baigashad.in@gmail.com>
2025-10-19 03:57:50 +0530

    Add files via upload

---
+++ Program_64.c
@@ -0,0 +1,40 @@
+// WAP to print an inverted pyramid having with user input n.
+
+/*
+
+*****************
+ ***************
+  *************
+   ***********
+    *********    
+     *******
+      *****
+       ***
+        *
+
+*/
+
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter an number : ");
+    scanf("%d",&n);
+    printf("\n");
+
+    for(int i = 1; i<=(2*n-1); i++)
+    {
+        for(int j=1; j<=(2*n-1); j++)
+        {
+            if(j>=i && j <= ((2*n-1+1-i)))
+            {
+                printf("%c",'*');
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
