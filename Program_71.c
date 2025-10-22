0a88dc0f66a967b992fa77ce2ae4ce0356e373d0
baigashad-in <baigashad.in@gmail.com>
2025-10-22 09:37:16 +0530

    Add files via upload

---
+++ Program_71.c
@@ -0,0 +1,37 @@
+// Array
+
+
+// WAP to input 10 values and print all values in a row one by one.
+// WAP to input 10 values and print all values in descending order of index.
+// WAP to input 10 elements in an array and print largest element of array.
+// WAP to input 10 elements of array and print smallest element of array.
+// WAP to input 5 subjects marks in an array and print total and percentages.
+// WAP to store 10 elements in an array and sort the elements of array in ascending order.
+// WAP to store 10 elements in an array and sort the elements of array in descending order.
+// WAP to store 10 elements in an array and check a given element is present in array or not by using linear search.
+// WAP to store 10 elements in an array and check a given element is present in array or not using binary search.
+// WAP to store 10 elements in an array and find index off a given statement.
+// WAP to store 10 elements in an array and check how many times a given element is present in array.
+// WAP to store 10 elements in an array and print only unique elements of the array.
+// WAP to input 10 elements of array and print number of occurrence of every distinct element has occurred.
+// WAP to input 10 elements in array and count how many even and odd elements are present in array.
+// WAP to input 10 elements of array and find sum of all even indexed elements and odd indexed elements.
+
+
+// WAP to input 10 elements of array and print pair of element for a given sum. (input sum = element1 + element2)
+// WAP to input 10 elements of array and count total zeros, even, odd present in array
+// WAP to in put 10 elements of array and print all indexes of a given element
+// WAP to input 10 elements of array and print how many elements are divisible by 3 and 5.
+
+
+
+#include<stdio.h>
+void main()
+{
+    int a[10];
+    for (int i=0; i<=10; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("a[5] = %d", a[5]);
+}
